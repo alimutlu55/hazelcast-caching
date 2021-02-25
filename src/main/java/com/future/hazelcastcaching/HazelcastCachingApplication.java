7a88782afe736f5cf20b89b3e6d0883c218853fc
almtl55 <alimutlu_5529@icloud.com>
2021-02-25 15:47:51 +0300

    initial commit

---
+++ src/main/java/com/future/hazelcastcaching/HazelcastCachingApplication.java
@@ -0,0 +1,13 @@
+package com.future.hazelcastcaching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HazelcastCachingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HazelcastCachingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/future/hazelcastcaching/HazelcastCachingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.future.hazelcastcaching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HazelcastCachingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
